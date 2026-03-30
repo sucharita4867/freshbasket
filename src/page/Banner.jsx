b43d51e2738c6ebbb543f0779c2ce0270e2f3a77
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-30 23:34:14 +0530

    added banner title

--- src/page/Banner.jsx
+++ src/page/Banner.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return <div>this is banner</div>;
 };
 
 export default Banner;
